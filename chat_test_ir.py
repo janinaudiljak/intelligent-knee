b50a5f790ac0e99ebd87af51d2da36a2140a13d3
janinaudiljak <janina@udiljak.com>
2024-05-02 10:38:05 +0200

    cahenges in comments

--- chat_test_ir.py
+++ chat_test_ir.py
@@ -1,41 +1,31 @@
 import gpiod
 import time
 
-# Set up GPIO using chip name and line offset
-chipname = "gpiochip0"  # This is usually gpiochip0 on Raspberry Pi
-IR_PIN_OFFSET = 18  # GPIO pin number
+chipname = "gpiochip0" 
+IR_PIN_OFFSET = 17
 
-# Open GPIO chip
 chip = gpiod.Chip(chipname)
 
-# Get the GPIO line
 line = chip.get_line(IR_PIN_OFFSET)
 
-# Request the line and configure it for input
 line.request(consumer="ir_sensor", type=gpiod.LINE_REQ_DIR_IN)
 
-# Function to be called when interrupt is triggered
 def on_color_change(event_type, offset):
-    if event_type == gpiod.LINE_EVENT_RISING_EDGE:  # If input is HIGH (white color)
+    if event_type == gpiod.LINE_EVENT_RISING_EDGE:
         print("White color detected! Halting...")
-        # Put your code here to halt/stop whatever process you need to interrupt
-        # For example, you can raise an exception to stop the program:
+       
         raise KeyboardInterrupt
-    else:  # If input is LOW (black color)
+    else:  
         print("Black color detected.")
 
-# Add event detection to the GPIO line
+
 line.request_event(gpiod.LINE_EVENT_RISING_EDGE | gpiod.LINE_EVENT_FALLING_EDGE, callback=on_color_change)
 
 try:
     while True:
-        # Main program loop
-        # You can put your main code logic here
-        # This loop will be interrupted when a color change is detected
-        time.sleep(0.1)  # Adjust the sleep time as needed
+        time.sleep(0.1) 
 except KeyboardInterrupt:
     print("Program halted by user or color change.")
 finally:
-    # Clean up GPIO
     line.release()
     chip.close()
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import gpiod
 import time
-test = 10
-while 1:
-    test += 1
-    time.sleep(1)
-
+chip = gpiod.Chip('gpiochip4')
+data_ir_sen = chip.get_line(17)
+data_ir_sen.request(consumer="IR_SEN", type=gpiod.LINE_REQ_DIR_OUT)
+try:
+   while True:
+       data_ir_sen.set_value(1)
+       time.sleep(1)
+       data_ir_sen.set_value(0)
+       time.sleep(1)
+finally:
+   data_ir_sen.release()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
