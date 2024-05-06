f099ec4f91f0d6060e84415175f1f58d721d528e
janinaudiljak <janina@udiljak.com>
2024-05-06 16:47:30 +0200

    changes

--- ADS.py
+++ ADS.py
@@ -5,10 +5,10 @@ import Adafruit_ADS1x15
 
 adc = Adafruit_ADS1x15.ADS1015(address=0x48, busnum=1)
 # Choose a gain of 1 for reading voltages from 0 to 4.09V.
-GAIN0 = 1
-GAIN1 = 1
-GAIN2 = 1
-GAIN3 = 1
+GAIN0 = 2
+GAIN1 = 2
+GAIN2 = 2
+GAIN3 = 2
 
 #To boost small signals, the gain can be adjusted on the ADS1x15 chips in the following steps:
 #GAIN_TWOTHIRDS (for an input range of +/- 6.144V)
@@ -23,7 +23,7 @@ while True:
     # print("pin0: ", values0)
     # time.sleep(0.1)
 
-    values1 = adc.read_adc(1, gain=GAIN1) #1518-(-1) konstig(!!)
+    values1 = adc.read_adc(1, gain=GAIN1) #1652-
     # print("pin1: ", values1)
     # time.sleep(0.1)
 
@@ -31,7 +31,7 @@ while True:
     # print("pin2: ", values2)
     # time.sleep(0.1)
 
-    values3 = adc.read_adc(3, gain=GAIN3) #254-222
+    values3 = adc.read_adc(3, gain=GAIN3) #254-222 KONSTIG!
     # print("pin3: ", values3)
     # time.sleep(0.1)
 
